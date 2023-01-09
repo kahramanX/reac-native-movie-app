b6915a47070c2e380d42e454a8077aae08dbc12a
Kahraman <80291704+kahramanX@users.noreply.github.com>
2023-01-09 10:46:04 +0300

    selected movie added to favorites

--- src/components/screens/favoritesScreen/FavoritesScreen.js
+++ src/components/screens/favoritesScreen/FavoritesScreen.js
@@ -1,7 +1,12 @@
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import MovieCard from "../../MovieCard";
 
 export default function FavoritesScreen() {
+  const { movieIDStore } = useSelector((state) => state.actionSlice);
+  const [getFavoritesMovie, setGetFavoritesMovie] = useState([]);
+
   const styles = StyleSheet.create({
     container: {
       backgroundColor: "#000000",
@@ -16,10 +21,42 @@ export default function FavoritesScreen() {
       paddingTop: 28,
     },
   });
+
+  const _getMoviesFromAPI = (idFromFavorites) => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${idFromFavorites}?api_key=9f2d1368e54e609b6d793560018b878a&language=en-US`
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        setGetFavoritesMovie((oldArray) => [...oldArray, data]);
+      });
+  };
+
+  useEffect(() => {
+    movieIDStore.forEach((movieID) => {
+      _getMoviesFromAPI(movieID);
+    });
+    console.log(getFavoritesMovie);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>My Favorite Movies</Text>
-      <Text style={{ color: "white" }}>test</Text>
+
+      <FlatList
+        data={getFavoritesMovie}
+        renderItem={({ item }) => {
+          return (
+            <MovieCard
+              movieID={item.id}
+              title={item.original_title}
+              imgUrl={item.poster_path}
+            />
+          );
+        }}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 }
